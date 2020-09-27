79ea4b6c2c978ff26844cc504ff2d245b5cb3988
Frank Force <frankforce@gmail.com>
2020-09-27 10:39:01 -0500

    code cleanup
    remove reset button
    add save sprite button

--- ZzSprite.js
+++ ZzSprite.js
@@ -2,7 +2,13 @@
 
 'use strict';
 
-function ZzSprite(context, x, y, seed, size=16, mode=0, mutateSeed=0, colorSeed=0)
+// ==ClosureCompiler==
+// @compilation_level ADVANCED_OPTIMIZATIONS
+// @language_out ECMASCRIPT_2019
+// @js_externs ZzSprite
+// ==/ClosureCompiler==
+
+function ZzSprite(context, x=0, y=0, seed=1, size=16, mode=0, mutateSeed=0, colorSeed=0)
 {
     function Random(max=1, min=0)
     {
@@ -13,16 +19,16 @@ function ZzSprite(context, x, y, seed, size=16, mode=0, mutateSeed=0, colorSeed=
 
     // random chance to flip drawing axis
     let randomSeed = seed;
-    const flipAxis = Random()<.5;
-    const w =  flipAxis ? size-3 : size/2-1|0;
-    const h = !flipAxis ? size-3 : size/2-1|0;
+    const flipAxis = Random() < .5;
+    const w =  flipAxis ? size-3 : size/2 - 1 |0;
+    const h = !flipAxis ? size-3 : size/2 - 1 |0;
         
     // apply mutations
-    randomSeed = seed+mutateSeed+1e8;
-    const spriteSize = size * Random(.9,.6);
-    const density = Random(1,.9);
-    const doubleCenter = Random()<.5;
-    const yBias = Random(.1,-.1);
+    randomSeed += mutateSeed + 1e8;
+    const spriteSize = size * Random(.9, .6);
+    const density = Random(1, .9);
+    const doubleCenter = Random() < .5;
+    const yBias = Random(.1, -.1);
     const colorRand = (mode==1 ? .08 : .04);
 
     // recenter
@@ -44,11 +50,11 @@ function ZzSprite(context, x, y, seed, size=16, mode=0, mutateSeed=0, colorSeed=
             const saveSeed = randomSeed;
             randomSeed += colorSeed + 1e9;
             const r = Random(360)|0;
-            let newColor = `hsl(${r},${Random(200,0)|0}%,${Random(100,20)|0}%)`;
+            let newColor = `hsl(${ r },${ Random(200,0)|0 }%,${ Random(100,20)|0 }%)`;
             if (outline || mode == 3)
                 newColor = '#000';
             else if (mode == 1)
-                newColor = r%2==1? '#777' : '#fff';
+                newColor = r%2? '#777' : '#fff';
             else if (mode == 2)
                 newColor = `#fff`;
             if (!k || Random() < colorRand)
@@ -59,9 +65,9 @@ function ZzSprite(context, x, y, seed, size=16, mode=0, mutateSeed=0, colorSeed=
             const isHole = Random() > density;
             if (Random(spriteSize/2)**2 > i*i + (j-(1-2*yBias)*h/2)**2 && !isHole)
             {
-                const o = outline;
-                context.fillRect(x+i-o-doubleCenter,y+j-o,1+2*o,1+2*o);
-                context.fillRect(x-i-o,y+j-o,1+2*o,1+2*o);
+                const o = !!outline;
+                context.fillRect(x+i-o-doubleCenter, y+j-o, 1+2*o, 1+2*o);
+                context.fillRect(x-i-o, y+j-o, 1+2*o, 1+2*o);
             }
         }
     }
@@ -69,5 +75,5 @@ function ZzSprite(context, x, y, seed, size=16, mode=0, mutateSeed=0, colorSeed=
     // outline then fill
     if (mode != 3)
         DrawSpriteInternal(x, y, 1);
-    DrawSpriteInternal(x, y, 0);
+    DrawSpriteInternal(x, y);
 }
